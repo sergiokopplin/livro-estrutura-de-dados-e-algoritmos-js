8d4c2ea2ad451bd56852980fbb9dcc638cca5bcc
Sergio Kopplin <sergiokopplin@gmail.com>
2022-02-06 08:26:24 -0300

    test: push method on Stack Object

--- src/stack-objects/stack.spec.ts
+++ src/stack-objects/stack.spec.ts
@@ -7,4 +7,19 @@ describe('Stack', () => {
     expect(stack.count).toBe(0);
     expect(stack.items).toEqual({});
   });
+
+  test('Should push element to stack and increment count', () => {
+    const stack = new Stack();
+
+    stack.push(12345);
+    stack.push(23456);
+    stack.push(34567);
+
+    expect(stack.items).toEqual({
+      0: 12345,
+      1: 23456,
+      2: 34567,
+    });
+    expect(stack.count).toEqual(3);
+  });
 });
--- src/stack-objects/stack.ts
+++ src/stack-objects/stack.ts
@@ -6,4 +6,9 @@ export class Stack {
     this.count = 0;
     this.items = {};
   }
+
+  push(element) {
+    this.items[this.count] = element;
+    this.count++;
+  }
 }
