f1e817e4244220e3642a993b3ee7d6aa429ce446
Sergio Kopplin <sergiokopplin@gmail.com>
2022-02-06 08:23:32 -0300

    test: init Stack Object properly

--- src/stack-objects/stack.spec.ts
+++ src/stack-objects/stack.spec.ts
@@ -1,9 +1,10 @@
 import { Stack } from './stack';
 
 describe('Stack', () => {
-  test('Should init ...', () => {
+  test('Should init empty and count zero', () => {
     const stack = new Stack();
 
-    expect(stack).toBeTruthy();
+    expect(stack.count).toBe(0);
+    expect(stack.items).toEqual({});
   });
 });
--- src/stack-objects/stack.ts
+++ src/stack-objects/stack.ts
@@ -1,3 +1,9 @@
 export class Stack {
-  constructor() {}
+  public items: { [key: number]: string };
+  public count: number;
+
+  constructor() {
+    this.count = 0;
+    this.items = {};
+  }
 }
