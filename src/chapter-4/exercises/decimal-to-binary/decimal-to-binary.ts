9c3664215d8d0330a9aa03d073bcc2c36567b38f
Sergio Kopplin <sergiokopplin@gmail.com>
2022-02-06 09:31:15 -0300

    refactor: stack decimalToBinary exercise

--- src/chapter-4/exercises/decimal-to-binary/decimal-to-binary.ts
+++ src/chapter-4/exercises/decimal-to-binary/decimal-to-binary.ts
@@ -2,13 +2,12 @@ import { Stack } from '../../stack-objects/stack';
 
 export function decimalToBinary(decNumber: number): string {
   const stack = new Stack();
-  let binaryString: string = '';
+
+  let binaryString = '';
   let number = decNumber;
-  let rem: number;
 
   while (number > 0) {
-    rem = Math.floor(number % 2);
-    stack.push(rem);
+    stack.push(Math.floor(number % 2));
     number = Math.floor(number / 2);
   }
 
