08ab99492e3052575c77b0235f8ff4a9ead9a0c8
Sergio Kopplin <sergiokopplin@gmail.com>
2022-02-10 20:06:26 -0300

    feat: adds palindrome exercise

---
+++ src/chapter-5/exercises/palindrome/palindrome.spec.ts
@@ -0,0 +1,16 @@
+import { palindrome } from './palindrome';
+
+describe('palindrome', () => {
+  test.only('Should integrate', () => {
+    const palindromeWords = ['aibofobia', 'omissíssimo', 'sopapos'];
+    const notPalindromeWords = ['John', 'Jack', 'Carl'];
+
+    expect(palindrome(palindromeWords[0])).toEqual(true);
+    expect(palindrome(palindromeWords[1])).toEqual(true);
+    expect(palindrome(palindromeWords[2])).toEqual(true);
+
+    expect(palindrome(notPalindromeWords[0])).toEqual(false);
+    expect(palindrome(notPalindromeWords[1])).toEqual(false);
+    expect(palindrome(notPalindromeWords[2])).toEqual(false);
+  });
+});
